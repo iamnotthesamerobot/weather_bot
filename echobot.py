69294b6e9bb9d6b2952fad443454b7a0c3cd6e7c
iamnotthesamerobot <42997264+iamnotthesamerobot@users.noreply.github.com>
2019-02-13 17:23:51 +0200

    Update echobot.py
    webhook added

--- echobot.py
+++ echobot.py
@@ -4,89 +4,39 @@
 Created on Fri Feb  8 17:29:46 2019
 
 """
-#script for Get last update
- 
+import os
 
-#TOKEN = '628611026:AAHrryBXhR5Y7OYjFEeQMFHo-KMVsuciBoY'
-url = "https://api.telegram.org/bot628611026:AAHrryBXhR5Y7OYjFEeQMFHo-KMVsuciBoY/"
+from flask import Flask, request
+
+import telebot
+
+TOKEN = '628611026:AAHrryBXhR5Y7OYjFEeQMFHo-KMVsuciBoY'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-import requests  
-import datetime
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-class BotHandler:
 
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-#        print('def __init__')
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-        print(str(result_json))
 
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-#        print(resp)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
-    def get_last_update(self):
-        get_result = self.get_updates()
-#        print('get_result' + str( get_result))
 
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-#            print('last_update' + str(last_update))
-        else:
-            last_update = get_result[len(get_result)]
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    return "!", 200
 
-        return last_update
-#        print(last_update)
-    
-greet_bot = BotHandler('628611026:AAHrryBXhR5Y7OYjFEeQMFHo-KMVsuciBoY')  
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-now = datetime.datetime.now()
 
-def main():  
-    new_offset = None
-    today = now.day
-    hour = now.hour
-    print('def main')
-
-    while True:
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-            
-        greet_bot.send_message(last_chat_id, 'You have inputted \"{}\"'.format(last_chat_text)) 
-
-        new_offset = last_update_id + 1
-
-if __name__ == '__main__':  
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
-    
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
