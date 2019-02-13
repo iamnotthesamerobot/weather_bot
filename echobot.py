2af33301b3bcbd7d8759fd23f41ec42e79c67263
iamnotthesamerobot <42997264+iamnotthesamerobot@users.noreply.github.com>
2019-02-13 18:39:21 +0200

    Update echobot.py
    print(message.text) added to 26 string

--- echobot.py
+++ echobot.py
@@ -23,6 +23,7 @@ def start(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
+    print(message.text)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
