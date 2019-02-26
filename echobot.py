d9ae3fe1b731db875f85d8545e403585d9d7331a
iamnotthesamerobot <42997264+iamnotthesamerobot@users.noreply.github.com>
2019-02-26 10:00:47 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -8,6 +8,10 @@ import os
 from flask import Flask, request
 import telebot
 
+import requests
+from bs4 import BeautifulSoup
+import time 
+
 TOKEN = '709643552:AAHfwkI5IFT5i7QGjDDvdjW5nMVUolnKL4o'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
@@ -16,7 +20,10 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-
+@bot.message_handler(func=lambda message: True, regexp="Z")
+def echo_message(message):
+    bot.reply_to(message, "YES!")
+    
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
