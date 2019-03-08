cd1298d9af538d4492e29e51e23c000f7d9dd003
iamnotthesamerobot <42997264+iamnotthesamerobot@users.noreply.github.com>
2019-03-08 10:33:08 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -24,7 +24,9 @@ def start(message):
 def echo_message(message):
     bot.reply_to(message, message.text)
  
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+#@bot.message_handler(func=lambda message: True, content_types=['text'])
+
+@bot.message_handler(func=lambda message: True, regexp="W")
 def echo_message(message):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
@@ -32,50 +34,23 @@ def echo_message(message):
 
     page_w = requests.get("https://www.gismeteo.com/weather-dnipro-5077/3-days/", headers=headers)
     soup_w = BeautifulSoup(page_w.content, 'html.parser')
-
-    def cloudy(i):
-        aa = str(soup_w.find_all(class_="img")[i].find(class_="tooltip"))
-        aa = re.sub(';|&', '#', aa, count=0).split(r'#')[4]
-        return aa
-
-    print(soup_w.find_all('h1')[0].contents[0]) #weather in Dn
-    bot.send_message(message.chat.id, str(soup_w.find_all('h1')[0].contents[0]))
-    for i in range(1, 13):
-        n = i - 1
-        N = 2
-        if i == 1:
-            day = '\n' + str(soup_w.find_all(class_="link blue")[2].contents[0]) + '\nNight   '
-        elif i == 5:
-            day = '\n' + str(soup_w.find_all(class_="link blue")[3].contents[0]) + '\nNight   '
-        elif i == 9:
-            day = '\n' + str(soup_w.find_all(class_="link blue")[4].contents[0]) + '\nNight   '
-        elif i == 2 or i == 6 or i == 10:
-            day = 'Morning '
-        elif i == 3 or i == 7 or i == 11:
-            day = 'Day     '
-        else:
-            day = 'Evening '
-        msg_1 = (day + str(soup_w.find_all(class_="unit unit_temperature_c")[n].contents[0]) + ' ' + str(cloudy(i)))
-        bot.send_message(message.chat.id, str(msg_1))
-
-    '''
-
-    msg_1 = (str(city) + ' ' + str(cur_date))
-    msg_2 = ('Actual ' + str(temp_now) + ' Feeling ' + str(temp_feel) + \
-             '\nNight ' + str(temp[0].contents[0]) + ' Day ' + str(temp[1].contents[0]))
-   # msg_3 = ('Road condition by 8 A.M.: ' + str(road_cond_8) + \
-    #         '\nRoad condition by 8 P.M.: ' + str(road_cond_20) + \
-     #        '\nWind speed: ' + str(wind_km_h) + ' km/h')
-    msg_4 = (str(sunrise) + ' ' + str(sunset) + '\n' + str(d_lenght) + '\n' + str(longer))
-    msg_5 = ('Moon: ' + str(moonrise) + ' ' + str(moonset) + '\n' + str(f_moon) + '\n' + str(moon_phase))
-          
-    bot.send_message(message.chat.id, str(msg_1))
-    bot.send_message(message.chat.id, str(msg_2))
-   # bot.send_message(message.chat.id, str(msg_3))
-    bot.send_message(message.chat.id, str(msg_4))
-    bot.send_message(message.chat.id, str(msg_5))
  
- '''
+    for i in range(0, 12):
+        if i == 0:
+            msg_0 = ('\n' + str(soup_w.select('a')[17].text))
+            bot.send_message(message.chat.id, str(msg_0))
+        if i == 4:
+            msg_0 = ('\n' + str(soup_w.select('a')[18].text))
+            bot.send_message(message.chat.id, str(msg_0))
+        if i == 8:
+            msg_0 = ('\n' + str(soup_w.select('a')[19].text))
+            bot.send_message(message.chat.id, str(msg_0))
+        day = soup_w.find_all(class_="time_of_day")[i].text
+        tmpr = soup_w.find_all(class_="unit unit_temperature_c")[i].contents[0]
+        cloudy = str(soup_w.find_all(class_="tooltip")[i])
+        cloudy = re.sub(';|&', '$', cloudy, count=0).split(r'$')
+        msg_1 = (str(day) + ' ' + str(tmpr) + ' ' + cloudy[4])  
+        bot.send_message(message.chat.id, str(msg_1))
  
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
